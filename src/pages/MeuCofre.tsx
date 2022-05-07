c0553ce2819e6f28823086d28768250e29608fab
felipe <felipecostagomes@unipam.edu.br>
2022-05-06 23:25:24 -0300

    DEV-login-route

--- src/pages/MeuCofre.tsx
+++ src/pages/MeuCofre.tsx
@@ -1,14 +1,112 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
 import { RiAddFill } from "react-icons/ri";
 import { Container } from "../components/Container";
 import { ItemCofre } from "../components/ItemCofre";
+import { folderService } from "../services";
 
 export default function MeuCofre() {
-  const testeItem = [{nome: 'Streamings'}, {nome: 'Redes Sociais'}, {nome: 'Faculdade'}]
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const testeItem = [
+    { nome: "Streamings" },
+    { nome: "Redes Sociais" },
+    { nome: "Faculdade" },
+  ];
+  const handleFolder = () => {
+    if (!title && !description) {
+    } else {
+      folderService.create({
+          title,
+          description,
+        })
+        .then((res) => {
+          const id = "toast-success-folder";
+            if (!toast.isActive(id)) {
+              toast({
+                id,
+                title: "Nova pasta adicionada!",
+                status: "success",
+                isClosable: true,
+              });
+            }
+        })
+        .catch((err) => {
+          const id = "toast-fail-folder";
+          if (!toast.isActive(id)) {
+            toast({
+              id,
+              title: "Falha ao cadastrar nova pasta",
+              status: "error",
+              isClosable: true,
+            });
+          }
+        });
+    }
+  };
   return (
-    <Container title="Meu Cofre" labelButton="Adicionar" iconButton={RiAddFill} buttonFunction={()=>{}}>
-      {testeItem.map((item, index)=>(
-        <ItemCofre key={`index-${index}`} title={item.nome} buttonFunction={()=>{}}/>
-      ))}
-    </Container>
+    <>
+      <Container
+        title="Meu Cofre"
+        labelButton="Adicionar"
+        iconButton={RiAddFill}
+        buttonFunction={onOpen}
+      >
+        {testeItem.map((item, index) => (
+          <ItemCofre
+            key={`index-${index}`}
+            title={item.nome}
+            buttonFunction={() => {}}
+          />
+        ))}
+      </Container>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Nova Pasta</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Nome da pasta</FormLabel>
+              <Input
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Nome da pasta"
+              />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Descrição</FormLabel>
+              <Input
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Descrição"
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button bg="green.700" mr={3} onClick={handleFolder}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+export * from './api'
+export * as userService from './user.service'
+export * as folderService from './folder.service'
+export * as authService from './auth.service'
